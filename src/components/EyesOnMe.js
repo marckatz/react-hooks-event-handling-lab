e86cfb6ae389810989a04f883fe46a7278f89e9e
Marc Katz <marc.f.katz@gmail.com>
2023-06-08 20:19:58 -0400

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function blurHandler(){
+        console.log("Hey! Eyes on me!")
+    }
+    function focusHandler(){
+        console.log("Good!")
+    }
+    return (
+        <div>
+            <button onBlur={blurHandler} onFocus={focusHandler}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
